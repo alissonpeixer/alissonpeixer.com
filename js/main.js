7d89c66768a4cc8c2683dea5a4d567ad109f0ece
Alisson Peixer <alissonpeixer4@gmail.com>
2022-09-19 21:33:49 -0300

    feat: frist commit new version

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 2000,
+}).reveal(`
+  #vanta,
+  #main,
+  #abountme, 
+  #abountme .section_container, 
+  #project .section_container,
+  footer
+`)
+
+
