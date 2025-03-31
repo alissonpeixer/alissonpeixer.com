8ca4080a95a392bd3898b0ca9ca0b71aee25cc6f
alissonpeixer <alissonpeixer4@gmail.com>
2025-03-30 23:24:54 -0300

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,265 @@
+"use client";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Menu } from "lucide-react";
+import { ReactTyped } from "react-typed";
 import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+  const texts = [
+    "Desafios são oportunidades para criar soluções incríveis.",
+    "Traga seus desafios, eu trago as soluções em código.",
+    "Do desafio ao código, a solução sempre aparece.",
+    "Transformando problemas em soluções escaláveis e eficientes.",
+    "Me mostre o problema, e eu entrego a solução.",
+  ];
+
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    section?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <main className={`h-screen bg-stone-900 text-white flex flex-col items-center p-5  ${menuOpen ? 'overflow-x-hidden' : 'overflow-x-scroll'}`}>
+      {/* header */}
+      <header className="flex items-center justify-between p-4 bg-stone-800 fixed left-1/2 transform -translate-x-1/2 w-full container mx-auto rounded-2xl z-10 mt-2 bg-opacity-50 backdrop-blur-lg shadow-md">
+        <h1 className="text-2xl font-bold">
+          <Image
+            src="/logo-a.svg"
+            alt="Logo"
+            width={44}
+            height={44}
+            className="object-contain"
+          />
+        </h1>
+        <button className="xl:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          <Menu size={24} />
+        </button>
+        <nav className="hidden xl:flex space-x-4">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="#home"
+            className="hover:text-gray-400"
+            onClick={(e) => {
+              e.preventDefault();
+              scrollToSection("home");
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Home
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="#about-me"
+            className="hover:text-gray-400"
+            onClick={(e) => {
+              e.preventDefault();
+              scrollToSection("about-me");
+            }}
           >
-            Read our docs
+            Sobre
           </a>
+          <a
+            href="#contact"
+            className="hover:text-gray-400"
+            onClick={(e) => {
+              e.preventDefault();
+              scrollToSection("contact");
+            }}
+          >
+            Contato
+          </a>
+        </nav>
+      </header>
+
+      {/* mobile menu */}
+      {/* {menuOpen && (
+        
+      )} */}
+
+      <nav className={`transition-all backdrop-blur-3xl opacity-30 text-center space-y-5 xl:hidden bg-stone-800 p-4 flex flex-col items-center justify-center   z-[100] w-[100%] rounded-4xl container top-28 fixed h-[80vh] transform ${menuOpen ? 'left-0 opacity-100' : 'left-[-500px] opacity-0'} ease-in-out`}>
+        <a
+          href="#home"
+          className="border p-10  w-full border-stone-600 rounded-2xl block py-2 text-white text-xl hover:text-gray-400 transition duration-300 transform hover:scale-105"
+          onClick={(e) => {
+            e.preventDefault();
+            scrollToSection("home");
+          }}
+        >
+          Sobre
+        </a>
+        <a
+          href="#about-me"
+          className="border p-10  w-full border-stone-600 rounded-2xl block py-2 text-white text-xl hover:text-gray-400 transition duration-300 transform hover:scale-105"
+          onClick={(e) => {
+            e.preventDefault();
+            scrollToSection("about-me");
+          }}
+        >
+          Projetos
+        </a>
+        <a
+          href="#contact"
+          className="border p-10  w-full border-stone-600 rounded-2xl block py-2 text-white text-xl hover:text-gray-400 transition duration-300 transform hover:scale-105"
+          onClick={(e) => {
+            e.preventDefault();
+            scrollToSection("contact");
+          }}
+        >
+          Contato
+        </a>
+      </nav>
+
+
+      {/* home section */}
+      <section
+        id="home"
+        className="flex flex-col xl:flex-row items-center min-h-[100svh] container justify-center xl:justify-between"
+        style={{ scrollSnapAlign: "start" }}
+      >
+        <div className="xl:w-1/2 text-center xl:text-left">
+          <motion.div
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1 }}
+            className="mb-6"
+          >
+            <Badge className="bg-stone-800">Desenvolvedor FullStack</Badge>
+          </motion.div>
+          <motion.h1
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1 }}
+            className="text-4xl xl:text-6xl text-amber-50 mb-6 font-bold"
+          >
+            Olá, sou o Alisson Peixer
+          </motion.h1>
+          <motion.h3
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1 }}
+            className="text-2xl xl:text-2xl mb-4 text-amber-100"
+          >
+            <ReactTyped strings={texts} typeSpeed={70} loop />
+          </motion.h3>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <motion.div
+          className="hidden xl:block"
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
         >
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/profile.svg"
+            alt="Alisson Peixer"
+            width={400}
+            height={400}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        </motion.div>
+      </section>
+
+      {/* about me*/}
+      <section
+        id="about-me"
+        className="flex flex-col xl:flex-row items-center min-h-screen container justify-center xl:justify-between py-20"
+        style={{ scrollSnapAlign: "start" }}
+      >
+        <motion.div
+          className="xl:w-1/2 text-center xl:text-left"
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <h2 className="text-4xl xl:text-5xl font-semibold text-amber-50 mb-4">
+            Sobre Mim
+          </h2>
+          <p className="text-lg xl:text-xl text-amber-100 mb-6">
+            Sou um desenvolvedor FullStack com paixão por criar soluções
+            inovadoras e eficientes. Com experiência tanto no front-end quanto
+            no back-end, estou sempre em busca de desafios que me permitam
+            crescer profissionalmente e entregar resultados de alta qualidade.
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="xl:w-1/2 hidden xl:block"
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          {/* <Image
+            src=""
+            alt="About Me"
+            width={400}
+            height={400}
+            className="rounded-lg shadow-lg"
+          /> */}
+        </motion.div>
+      </section>
+
+      {/* fale comigo */}
+      <section
+        id="contact"
+        className="flex flex-col items-center min-h-screen container justify-center py-20"
+      >
+        <motion.div  
+          className="flex flex-col space-y-7 text-center mb-10"
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+        >
+          <h2 className="text-4xl font-semibold text-amber-50 mb-4">Contato</h2>
+          <p className="text-lg xl:text-xl text-amber-100 mb-6">
+            Entre em contato comigo!
+          </p>
+          <a href="mailto:alisson.peixer@gmail.com" className="underline">
+            alisson.peixer@gmail.com
+          </a>
+        </motion.div>
+
+        <motion.div 
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+        >
+          <div className="flex space-x-4 mb-10">
+            Ou me envie uma mensagem pelo formulário abaixo:
+          </div>
+          <form
+            action="https://formspree.io/f/your-form-id"
+            method="POST"
+            className="w-full max-w-md"
+          >
+            <input type="hidden" name="_subject" value="Contato pelo site" />
+            <input
+              type="text"
+              name="name"
+              placeholder="Nome"
+              required
+              className="w-full p-4 mb-6 border rounded-md focus:outline-none"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+              className="w-full p-4 mb-6 border rounded-md focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Mensagem"
+              required
+              className="w-full p-4 mb-6 border rounded-md focus:outline-none"
+            />
+            <button
+              type="submit"
+              className="w-full p-4 bg-amber-500 text-white rounded-md hover:bg-amber-400"
+            >
+              Enviar
+            </button>
+          </form>
+        </motion.div>
+      </section>
+    </main>
   );
 }
